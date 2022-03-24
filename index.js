721ab28eecb8dbb0749ebdc90b386a500ae65eaf
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-24 10:31:01 -0400

    commit tree fb719c2e87b68d14533de243b5d16321d7624f45

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+const dodger= document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = "0px";
+dodger.style.left = "0px";
+
+document.addEventListener("keyDown", function (event) {
+    if (event.key === "ArrowLeft") {
+        const leftNumbers = dodger.style.leftReplace("px", "")
+        const left = parseInt(leftNumbers, 10);
+        dodger.style.left = `${left - 1}px`;
+    }
+})
+//notes
+//
